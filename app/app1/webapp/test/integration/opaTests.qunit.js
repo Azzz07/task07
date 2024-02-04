9ae23854a4bd5ceb69aed4fc5c2c1100b46ee3b1
Azzz07 <abdul.azeem@peolsolutions.com>
2024-02-04 18:08:25 +0000

    second commit

---
+++ app/app1/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,26 @@
+sap.ui.require(
+    [
+        'sap/fe/test/JourneyRunner',
+        'app1/test/integration/FirstJourney',
+		'app1/test/integration/pages/BooksList',
+		'app1/test/integration/pages/BooksObjectPage'
+    ],
+    function(JourneyRunner, opaJourney, BooksList, BooksObjectPage) {
+        'use strict';
+        var JourneyRunner = new JourneyRunner({
+            // start index.html in web folder
+            launchUrl: sap.ui.require.toUrl('app1') + '/index.html'
+        });
+
+       
+        JourneyRunner.run(
+            {
+                pages: { 
+					onTheBooksList: BooksList,
+					onTheBooksObjectPage: BooksObjectPage
+                }
+            },
+            opaJourney.run
+        );
+    }
+);
---
+++ app/services.cds
@@ -0,0 +1,3 @@
+
+
+using from './app1/annotations';
